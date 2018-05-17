cf26040e831f43e6a0eb48084d77003276fce92d
logonrm <logonrm@fiap.com.br>
2018-05-17 19:19:07 -0300

    first commit

---
+++ XF.Hello/XF.Hello/Configuracao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.Hello
+{
+    public class Configuracao
+    {
+        public bool RastrearLocalizacao { get; set; }
+        public bool PermitirCookies { get; set; }
+        public bool DisponibilizarNoMural { get; set; }
+        public bool EnviarPorEmail { get; set; }
+        public bool ReceberSms { get; set; }
+    }
+}
